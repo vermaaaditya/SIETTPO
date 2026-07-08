b239e604f3850ac17e7e7ae76b45030187eeacb7
unknown <vermaaadityaff123@gmail.com>
2026-07-08 12:08:50 +0530

    Remove redundant top brand logo

--- src/pages/StudentLogin.jsx
+++ src/pages/StudentLogin.jsx
@@ -360,7 +360,6 @@ export default function StudentLogin() {
 
         {/* Brand */}
         <div className="login-left-brand">
-          <img src="/images/newlogo.jpeg" alt="SIET Panchkula" />
           <div>
             <p className="login-left-brand-name">
               {t.brandName.split('\n').map((line, i) => (
